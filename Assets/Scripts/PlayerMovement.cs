a6d93071ad24ef1946da783ddab40012a89746d9
mrome007 <mrome007@ucr.edu>
2017-12-20 22:28:02 -0800

    New notebook background and player movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	private void Update()
+	{
+		var h = Input.GetAxis("Horizontal");
+		var v = Input.GetAxis("Vertical");
+
+		transform.Translate(new Vector2(h, v) * Time.deltaTime * 10f);
+	}
+}
