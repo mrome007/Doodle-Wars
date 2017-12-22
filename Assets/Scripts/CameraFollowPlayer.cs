71ad326611a7b6fa9028b121c1257f5dd2650707
mrome007 <mrome007@ucr.edu>
2017-12-21 21:43:58 -0800

    Basic player movement and camera follow. Add Sun and Cloud art

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform playerTransform;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector3 initialCamPos;
+    private Vector3 targetVector;
+    private void Awake()
+    {
+        initialCamPos = transform.position;
+        targetVector = initialCamPos;
+    }
+
+    private void Update()
+    {
+        targetVector.x = playerTransform.position.x;
+        transform.position = Vector3.MoveTowards(transform.position, targetVector, speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour 
 {
+    [SerializeField]
+    private Transform leftConstraint;
+
+    [SerializeField]
+    private Transform rightConstraint;
+
+    private Vector2 movementVector;
+    private Vector2 clampedPosition;
+    private float minX;
+    private float minY;
+    private float maxX;
+    private float maxY;
+
+    private void Awake()
+    {
+        movementVector = Vector2.zero;
+        clampedPosition = Vector2.zero;
+    }
+
 	private void Update()
 	{
-		var h = Input.GetAxis("Horizontal");
-		var v = Input.GetAxis("Vertical");
+        var h = Input.GetAxis("Horizontal");
+        movementVector.x = h;
+        transform.Translate(movementVector * Time.deltaTime * 10f);
+        ClampMovement();
+    }
 
-		transform.Translate(new Vector2(h, v) * Time.deltaTime * 10f);
-	}
+    private void ClampMovement()
+    {
+        minX = leftConstraint.position.x;
+        maxX = rightConstraint.position.x;
+        minY = leftConstraint.position.y;
+        maxY = rightConstraint.position.y;
+
+        var x = Mathf.Clamp(transform.position.x, minX, maxX);
+        var y = Mathf.Clamp(transform.position.y, minY, maxY);
+
+        clampedPosition.x = x;
+        clampedPosition.y = y;
+
+        transform.position = clampedPosition;
+    }
 }
