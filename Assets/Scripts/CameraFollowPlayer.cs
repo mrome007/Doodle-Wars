e47fcd68b2eac5b1c91fbe34c0ed5b7d6d667b2d
mrome007 <mrome007@ucr.edu>
2017-12-28 15:07:47 -0800

    Clamp Camera movement so it's more reminiscent of beat em up cameras

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -8,10 +8,20 @@ public class CameraFollowPlayer : MonoBehaviour
     private Transform playerTransform;
 
     [SerializeField]
+    private Transform leftWorldConstraint;
+
+    [SerializeField]
+    private Transform rightWorldConstraint;
+
+    [SerializeField]
     private float speed;
 
     private Vector3 initialCamPos;
     private Vector3 targetVector;
+    private float constraintOffset = 6f;
+    private float minX;
+    private float maxX;
+
     private void Awake()
     {
         initialCamPos = transform.position;
@@ -21,6 +31,15 @@ public class CameraFollowPlayer : MonoBehaviour
     private void Update()
     {
         targetVector.x = playerTransform.position.x;
+        ClampFollow();
         transform.position = Vector3.MoveTowards(transform.position, targetVector, speed * Time.deltaTime);
+    }
+
+    private void ClampFollow()
+    {
+        minX = leftWorldConstraint.position.x + constraintOffset;
+        maxX = rightWorldConstraint.position.x - constraintOffset;
+
+        targetVector.x = Mathf.Clamp(targetVector.x, minX, maxX);
     }
 }
